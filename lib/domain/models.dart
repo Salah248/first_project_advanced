98cee5cca803b7103707a6a806d67084374b205d
Salah248 <salahsadoon248@gmail.com>
2024-10-16 14:04:08 +0300

    Domain Layer - Creating Models

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -15,3 +15,39 @@ class SliderViewObject {
 
   SliderViewObject(this.sliderOpject, this.currentIndex, this.numOfSlides);
 }
+
+// login models
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotification;
+
+  Customer(
+    this.id,
+    this.name,
+    this.numOfNotification,
+  );
+}
+
+class Contacts {
+  String phone;
+  String email;
+  int link;
+
+  Contacts(
+    this.phone,
+    this.email,
+    this.link,
+  );
+}
+
+class Authentiction {
+  Customer customer;
+  Contacts contacts;
+
+  Authentiction(
+    this.contacts,
+    this.customer,
+  );
+}
