e65d65ffd73a37a9dc907f9998211491073ed1db
Salah248 <salahsadoon248@gmail.com>
2024-10-29 16:36:23 +0300

    Adding Implementation for (Error State-Empty State-Content State) Cases

--- lib/pressentaion/common/state_renderer/state_rendere_impl.dart
+++ lib/pressentaion/common/state_renderer/state_rendere_impl.dart
@@ -1,6 +1,7 @@
 import 'package:first_project_advanced/app/constants.dart';
 import 'package:first_project_advanced/pressentaion/common/state_renderer/state_renderer.dart';
 import 'package:first_project_advanced/pressentaion/resources/strings_manager.dart';
+import 'package:flutter/material.dart';
 
 abstract class FlowState {
   StateRendererType getStateRendererType();
@@ -58,4 +59,88 @@ class EmptyState extends FlowState {
   @override
   StateRendererType getStateRendererType() =>
       StateRendererType.fullScreenEmptyState;
+}
+
+extension FlowStateExtension on FlowState {
+  Widget getScreenWidget(
+    BuildContext context,
+    Widget contentScreenWidget,
+    Function retryActionFunction,
+  ) {
+    switch (runtimeType) {
+      case const (LoadingState):
+        {
+          if (getStateRendererType() ==
+              StateRendererType.fullScreenLoadingState) {
+            // show popup loading
+            showPopup(
+              context,
+              getStateRendererType(),
+              getMessage(),
+            );
+
+            // show content ui of screen
+            return contentScreenWidget;
+          } else {
+            // full screen  loading state
+            return StateRenderer(
+                message: getMessage(),
+                stateRendererType: getStateRendererType(),
+                retryActionFunction: retryActionFunction);
+          }
+        }
+      case const (ErrorState):
+        {
+          if (getStateRendererType() == StateRendererType.popupErrorState) {
+            // show popup error
+            showPopup(
+              context,
+              getStateRendererType(),
+              getMessage(),
+            );
+
+            // show content ui of screen
+            return contentScreenWidget;
+          } else {
+            // full screen  loading state
+            return StateRenderer(
+                message: getMessage(),
+                stateRendererType: getStateRendererType(),
+                retryActionFunction: retryActionFunction);
+          }
+        }
+      case const (EmptyState):
+        {
+          return StateRenderer(
+              message: getMessage(),
+              stateRendererType: getStateRendererType(),
+              retryActionFunction: retryActionFunction);
+        }
+      case const (ContentState):
+        {
+          return contentScreenWidget;
+        }
+      default:
+        {
+          return contentScreenWidget;
+        }
+    }
+  }
+
+  showPopup(
+    BuildContext context,
+    StateRendererType stateRendererType,
+    String message,
+  ) {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => showDialog(
+        context: context,
+        builder: (BuildContext context) => StateRenderer(
+          stateRendererType: stateRendererType,
+          message: message,
+          retryActionFunction: () {},
+        ),
+      ),
+    );
+  }
 }
